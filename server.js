fc77e4a6d0c892462614d0cf44a55bc826a3c226
Paul Lu <paul.l.sining@gmail.com>
2023-02-17 22:20:47 -0800

    before redis

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 /** @format */
 
-// No Redis Used
 const express = require("express");
 const axios = require("axios");
 
@@ -10,10 +9,20 @@ app.use(express.json());
 // Mimic an api call
 const postsUrl = "https://jsonplaceholder.typicode.com/posts/";
 
-// enter: http://localhost:3000/posts?id=1
+// enter: http://localhost:3000/posts
 app.get("/posts", async (req, res) => {
   try {
-    const { data } = await axios.get(postsUrl + req.query.id);
+    const { data } = await axios.get(postsUrl);
+    res.send(data);
+  } catch (error) {
+    res.status(400).send("Failed to fetch posts");
+  }
+});
+
+// enter: http://localhost:3000/posts/1
+app.get("/posts/:id", async (req, res) => {
+  try {
+    const { data } = await axios.get(postsUrl+req.params.id);
     res.send(data);
   } catch (error) {
     res.status(400).send("Failed to fetch posts");
